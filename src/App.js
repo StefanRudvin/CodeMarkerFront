9acf37bdb7b5821d696cd80637d15cb96c04b031
StefanRudvin <stefan.rudvin@hotmail.com>
2017-10-19 16:40:13 +0100

    Configure react router

--- src/App.js
+++ src/App.js
@@ -4,36 +4,74 @@ import logo from './logo.svg';
 // Bootstrap
 import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 // Components
 import Application from './Components/Application.js';
 import Assignment  from './Components/Assignment.js';
 import Login       from './Components/Login.js';
 import NavBar      from './Components/Navigation/NavBar.js';
 
+//  Views
+import Course      from './Views/Course.js';
+
 import './Css/App.css';
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
+// Each logical "route" has two components, one for
+// the sidebar and one for the main area. We want to
+// render both of them in different places when the
+// path matches the current URL.
+const routes = [
+  { path: '/',
+    exact: true,
+    main: () => <h2>Home</h2>
+  },
+  { path: '/login',
+    exact: true,
+    main: () => <Login name="NameProp"/>
+  },
+  { path: '/user',
+    main: () => <h2>User</h2>
+  },
+  { path: '/assignments',
+    main: () => <h2>Assignments</h2>
+  },
+  { path: '/courses',
+    main: () => <Course />
+  }
+]
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar/>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to CodeMarker</h1>
-        </header>
-        <p className="App-intro">
-          Codemarker is an application used by the University of Aberdeen to assess student submissions for code assignments.
-        </p>
-        <Welcome name="Stefan"/>
-        <Application/>
-        <Assignment/>
-        <Login name="NameProp"/>
+      <Router>
+        <div className="App">
+          <NavBar/>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to CodeMarker</h1>
+          </header>
 
-      </div>
+          {routes.map((route, index) => (
+            // Render more <Route>s with the same paths as
+            // above, but different components this time.
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.main}
+            />
+          ))}
+
+          <p className="App-intro">
+            Codemarker is an application used by the
+          </p>
+          <p className="App-intro">
+            University of Aberdeen to assess student submissions for code assignments.
+          </p>
+        </div>
+      </Router>
     );
   }
 }
--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -1,42 +1,43 @@
 import React from 'react';
 import { Navbar, Jumbotron, Button, MenuItem, NavItem, NavDropdown, Nav } from 'react-bootstrap';
 import Style from './../../Css/style.scss';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-class NavBar extends React.Component {
-  render() {
-    const navStyle = {
-      color: 'blue',
-      background: 'blue',
-      marginBottom: 0,
-    }
-    const testStyle = {
-      marginBottom: 0,
-      borderRadius: 0
-    }
-    return (
-      <div className="navBar" style={navStyle}>
-        <Navbar inverse collapseOnSelect style={testStyle}>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">CodeMarker</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav>
-              <NavItem eventKey={1} href="#">Courses</NavItem>
-            </Nav>
-            <Nav pullRight>
-              <NavItem eventKey={1} href="#">Profile</NavItem>
-              <NavItem eventKey={2} href="#">Logout</NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
-    );
+const NavBar = props => {
+  const navStyle = {
+    color: 'blue',
+    background: 'blue',
+    marginBottom: 0,
   }
-}
-
-
+  const testStyle = {
+    marginBottom: 0,
+    borderRadius: 0
+  }
+  return (
+    <div className="navBar" style={navStyle}>
+      <Navbar inverse collapseOnSelect style={testStyle}>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a><Link to="/">Codemarker</Link></a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1}><Link to="/courses">Courses</Link></NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">Profile</NavItem>
+            <NavItem eventKey={2} href="#">Logout</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
 
 export default NavBar;
