ba0654597c3c864d7f8ccf0da9c43af79fc491d8
StefanRudvin <stefan.rudvin@hotmail.com>
2017-11-09 01:34:39 +0000

    Display individual courses via API

--- src/Views/Course.js
+++ src/Views/Course.js
@@ -1,21 +1,37 @@
-import React from 'react';
 import { Button } from 'react-bootstrap';
+import React from 'react';
+import axios from 'axios';
 
 class Course extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      course: []
+      course: {}
     }
+    this.props.history.listen((location, action) => {
+      let id = location.pathname.slice(9);
+      this.getCourse(id);
+    });
+  }
+
+  getCourse(id){
+    let url = 'http://127.0.0.1:8000/api/courses/' + id + '/?format=json';
+    axios.get(url)
+      .then( (response) => {
+        return response.data})
+      .then( (json) => {
+        this.setState({course: json});
+      });
   }
 
   componentDidMount() {
-    console.log(this.props);
+    this.getCourse(this.props.match.params.id);
   }
   render() {
     return (
       <div className="login">
-        <h1>I am course #{this.props.match.params.id}!</h1>
+        <h1>{this.state.course.name}</h1>
+        <p>{this.state.course.description}</p>
 
         <Button active={false}>Helloo</Button>
         <Button active={true}>Helloo</Button>
