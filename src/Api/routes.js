ff63839ea79808edcaad6428d6f6b96a0402d9ed
Konrad Dryja <kdryja@gmail.com>
2018-03-09 18:21:20 +0000

    Change routes

--- src/Api/routes.js
+++ src/Api/routes.js
@@ -5,21 +5,21 @@ export default {
     },
 
     auth: {
-        obtain_token: 'http://0.0.0.0:8000/api/obtain-auth-token/',
-        get_user: 'http://0.0.0.0:8000/api/get-user/',
+        obtain_token: 'http://codermarker.uoa.abdn.ac.uk:8000/api/obtain-auth-token/',
+        get_user: 'http://codermarker.uoa.abdn.ac.uk:8000/api/get-user/',
     },
 
-    courses_json: 'http://0.0.0.0:8000/api/courses/?format=json',
-    users_json: 'http://0.0.0.0:8000/api/users/?format=json',
+    courses_json: 'http://codermarker.uoa.abdn.ac.uk:8000/api/courses/?format=json',
+    users_json: 'http://codermarker.uoa.abdn.ac.uk:8000/api/users/?format=json',
 
-    courses_users_delete: 'http://0.0.0.0:8000/api/courses/users/delete/',
-    courses_users_add: 'http://0.0.0.0:8000/api/courses/users/add/',
+    courses_users_delete: 'http://codermarker.uoa.abdn.ac.uk:8000/api/courses/users/delete/',
+    courses_users_add: 'http://codermarker.uoa.abdn.ac.uk:8000/api/courses/users/add/',
 
-    assessments: 'http://0.0.0.0:8000/api/assessments/',
-    submissions: 'http://0.0.0.0:8000/api/submissions/',
-    courses: 'http://0.0.0.0:8000/api/courses/',
-    users: 'http://0.0.0.0:8000/api/users/',
+    assessments: 'http://codermarker.uoa.abdn.ac.uk:8000/api/assessments/',
+    submissions: 'http://codermarker.uoa.abdn.ac.uk:8000/api/submissions/',
+    courses: 'http://codermarker.uoa.abdn.ac.uk:8000/api/courses/',
+    users: 'http://codermarker.uoa.abdn.ac.uk:8000/api/users/',
 
-    base: 'http://0.0.0.0:8000/api/',
+    base: 'http://codermarker.uoa.abdn.ac.uk:8000/api/',
 
 }
