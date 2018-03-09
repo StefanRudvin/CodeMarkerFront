1018e1a008a6362c5aa566da46a55c83c3b6621a
Konrad Dryja <kdryja@gmail.com>
2018-03-09 18:15:00 +0000

    Change routes for deployment server

--- src/Api/routes.js
+++ src/Api/routes.js
@@ -4,22 +4,22 @@ export default {
         assessments: 'http://localhost:3000/assessments/'
     },
 
-    auth : {
-        obtain_token : 'http://127.0.0.1:8000/api/obtain-auth-token/',
-        get_user: 'http://127.0.0.1:8000/api/get-user/',
+    auth: {
+        obtain_token: 'http://0.0.0.0:8000/api/obtain-auth-token/',
+        get_user: 'http://0.0.0.0:8000/api/get-user/',
     },
 
-    courses_json: 'http://127.0.0.1:8000/api/courses/?format=json',
-    users_json: 'http://127.0.0.1:8000/api/users/?format=json',
+    courses_json: 'http://0.0.0.0:8000/api/courses/?format=json',
+    users_json: 'http://0.0.0.0:8000/api/users/?format=json',
 
-    courses_users_delete: 'http://127.0.0.1:8000/api/courses/users/delete/',
-    courses_users_add: 'http://127.0.0.1:8000/api/courses/users/add/',
+    courses_users_delete: 'http://0.0.0.0:8000/api/courses/users/delete/',
+    courses_users_add: 'http://0.0.0.0:8000/api/courses/users/add/',
 
-    assessments: 'http://127.0.0.1:8000/api/assessments/',
-    submissions: 'http://127.0.0.1:8000/api/submissions/',
-    courses: 'http://127.0.0.1:8000/api/courses/',
-    users: 'http://127.0.0.1:8000/api/users/',
+    assessments: 'http://0.0.0.0:8000/api/assessments/',
+    submissions: 'http://0.0.0.0:8000/api/submissions/',
+    courses: 'http://0.0.0.0:8000/api/courses/',
+    users: 'http://0.0.0.0:8000/api/users/',
 
-    base: 'http://127.0.0.1:8000/api/',
+    base: 'http://0.0.0.0:8000/api/',
 
 }
