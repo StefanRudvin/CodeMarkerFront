e66a8e7d723f6fec7bc0a2bc00617613b18bb909
Konrad Dryja <kdryja@gmail.com>
2018-04-04 19:27:54 +0100

    Adjust backend URL

--- src/env.js
+++ src/env.js
@@ -2,7 +2,7 @@ const hostname = window && window.location && window.location.hostname;
 
 let url = "";
 if (hostname === "codemarker.uoa.abdn.ac.uk") {
-    url = "https://codemarker.uoa.abdn.ac.uk/api/";
+    url = "https://codemarker.uoa.abdn.ac.uk/backend/api/";
 }
 else {
     url = "http://127.0.0.1:8000/api/"
