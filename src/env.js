59c8099d11e95b929cf47d9ce68eca2e34a681b6
Konrad Dryja <kdryja@gmail.com>
2018-04-04 19:51:19 +0100

    Remove backend from urls

--- src/env.js
+++ src/env.js
@@ -2,10 +2,10 @@ const hostname = window && window.location && window.location.hostname;
 
 let url = "";
 if (hostname === "codemarker.uoa.abdn.ac.uk") {
-    url = "https://codemarker.uoa.abdn.ac.uk/backend/api/";
+    url = "https://codemarker.uoa.abdn.ac.uk/api/";
 }
 else {
-    url = "http://127.0.0.1:8000/api/"
+    url = "http://127.0.0.1:8000/"
 }
 
 export default {
