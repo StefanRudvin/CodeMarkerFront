720fb5ddcb70ebdec2e763a4d8003ec6a9dd24b8
Konrad Dryja <kdryja@gmail.com>
2018-04-04 19:18:26 +0100

    Move production API to https and to the same port

--- src/env.js
+++ src/env.js
@@ -2,7 +2,7 @@ const hostname = window && window.location && window.location.hostname;
 
 let url = "";
 if (hostname === "codemarker.uoa.abdn.ac.uk") {
-    url = "http://codemarker.uoa.abdn.ac.uk:8000/api/";
+    url = "https://codemarker.uoa.abdn.ac.uk/api/";
 }
 else {
     url = "http://127.0.0.1:8000/api/"
